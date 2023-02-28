1a89277fbe3755b109e99b63cab96cfc345d9565
jslee <jslee@plaif.com>
2023-02-28 01:36:21 +0000

    initial commit

---
+++ my_vision.py
@@ -0,0 +1,6 @@
+import rospy
+
+if __name__ == '__main__':
+    print('start my_vision..!')
+    rospy.init_node('my_vision')
+    rospy.spin()
---
+++ node_onoff_mgr.py
@@ -0,0 +1,44 @@
+import rospy
+import subprocess
+from std_msgs.msg import String
+
+def callback_vision_onoff_cmd(data):
+    node_name = data.data
+    print('callback_vision_onoff_cmd starts..!')
+    # turn off
+    if is_node_running(node_name) == True:
+        kill_cmd = 'rosnode kill ' + node_name
+        subprocess.call(kill_cmd.split())
+    # turn on
+    subprocess.Popen('python my_vision.py'.split())
+    print('callback_vision_onoff_cmd terminates..!')
+
+
+def callback_is_node_running(data):
+    node_name = data.data
+    is_running = is_node_running(node_name)
+    print(node_name + ' is running : ' + str(is_running))
+    pub = rospy.Publisher('is_node_running_result', String, queue_size=10)
+    pub.publish(str(is_running))
+    print('callback_is_node_running terminates..!')
+
+# check if node_name is running
+def is_node_running(node_name):
+    print('rosnode list :')
+    output = subprocess.check_output('rosnode list'.split())
+    print(output)
+    return True if node_name in output else False
+
+def listener():
+    rospy.init_node('node_onoff_mgr')
+
+    # topic : vision_onoff_cmd
+    # msg type : std_msgs/String
+    # data : {target_node_name}
+    rospy.Subscriber('vision_onoff_cmd', String, callback_vision_onoff_cmd)
+    rospy.Subscriber('is_node_running', String, callback_is_node_running)
+    rospy.spin()
+
+if __name__ == '__main__':
+    print('start node_onoff_mgr..!')
+    listener()
